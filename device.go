697e61c204a9109b36bdae9a51f7a5beab5e7de2
Georgi Valkov <georgi.t.valkov@gmail.com>
2012-09-17 15:30:54 +0300

    [wip]

--- device.go
+++ device.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"unsafe"
 	"strings"
+	"encoding/binary"
+	"bytes"
 )
 
 const MAX_NAME_SIZE = 256
@@ -16,14 +18,14 @@ type InputDevice struct {
 
 	Name string      // device name
 	Phys string      // physical topology of device
-	Fd   uintptr     // a non-blocking file descriptor to the device file
+	File *os.File    // an open file handle to the input device
 
-	Bustype uint16   // bustype identifier
+	Bustype uint16   // bus type identifier
 	Vendor  uint16   // vendor identifier
 	Product uint16   // product identifier
 	Version uint16   // version identifier
 
-	Capabilities map[int][]int  // supported event types and codes
+	Capabilities map[int][]int  // supported event types and codes.
 }
 
 func Open(devnode string) *InputDevice {
@@ -35,12 +37,45 @@ func Open(devnode string) *InputDevice {
 
 	dev := InputDevice{}
 	dev.Fn = devnode
-	dev.Fd = file.Fd()
+	dev.File = file
 
-	dev.get_device_info()
-	dev.get_device_capabilities()
+	dev.set_device_info()
+	dev.set_device_capabilities()
 
 	return &dev
+}
+
+// Read multiple input events from device
+func (dev *InputDevice) Read() []InputEvent {
+	events := make([]InputEvent, 16)
+	buffer := make([]byte, eventsize*16)
+
+	dev.File.Read(buffer)
+
+	b := bytes.NewBuffer(buffer)
+	binary.Read(b, binary.LittleEndian, &events)
+
+	for i := range events {
+		if events[i].Time.Sec == 0 {
+			events = append(events[:i])
+			break
+		}
+	}
+
+	return events
+}
+
+// Read and return a single input event
+func (dev *InputDevice) ReadOne() *InputEvent {
+	event := InputEvent{}
+	buffer := make([]byte, eventsize)
+
+	dev.File.Read(buffer)
+
+	b := bytes.NewBuffer(buffer)
+	binary.Read(b, binary.LittleEndian, &event)
+
+	return &event
 }
 
 func (dev *InputDevice) String() string {
@@ -59,30 +94,34 @@ func (dev *InputDevice) String() string {
 		"  phys %s\n" +
 		"  bus 0x%x, vendor 0x%x, product 0x%x, version 0x%x\n" +
 		"  events %s",
-		dev.Fn, dev.Fd, dev.Name, dev.Phys, dev.Bustype,
+		dev.Fn, dev.File.Fd(), dev.Name, dev.Phys, dev.Bustype,
 		dev.Vendor, dev.Product, dev.Version, evtypes_s)
 }
 
 // Gets the event types and event codes that the input device supports
-func (dev *InputDevice) get_device_capabilities() {
+func (dev *InputDevice) set_device_capabilities() {
+    // Capabilities is a map of supported event types to lists of
+    // events e.g: {1: [272, 273, 274, 275], 2: [0, 1, 6, 8]}
 	capabilities := make(map[int][]int)
 
 	evbits   := new([ (EV_MAX+1)/8]byte)
 	codebits := new([(KEY_MAX+1)/8]byte)
 
-	ioctl(dev.Fd, EVIOCGBIT(0, EV_MAX), unsafe.Pointer(evbits))
+	ioctl(dev.File.Fd(), EVIOCGBIT(0, EV_MAX), unsafe.Pointer(evbits))
 
+    // Build a map of the device's capabilities
 	for evtype := 0; evtype < EV_MAX; evtype++ {
 		if evbits[evtype/8] & (1 << uint(evtype % 8)) != 0 {
 			eventcodes := make([]int, 0)
 
-			ioctl(dev.Fd, EVIOCGBIT(evtype, KEY_MAX), unsafe.Pointer(codebits))
+			ioctl(dev.File.Fd(), EVIOCGBIT(evtype, KEY_MAX), unsafe.Pointer(codebits))
 			for evcode := 0; evcode < KEY_MAX; evcode++ {
 				if codebits[evcode/8] & (1 << uint(evcode % 8)) != 0 {
 					eventcodes = append(eventcodes, evcode)
 				}
 			}
 
+            // capabilities[EV_KEY] = [KEY_A, KEY_B, KEY_C, ...]
 			capabilities[evtype] = eventcodes
 		}
 	}
@@ -91,17 +130,15 @@ func (dev *InputDevice) get_device_capabilities() {
 }
 
 // An all-in-one function for describing an input device
-func (dev *InputDevice) get_device_info() {
+func (dev *InputDevice) set_device_info() {
 	info := device_info{}
 
 	name := new([MAX_NAME_SIZE]byte)
 	phys := new([MAX_NAME_SIZE]byte)
 
-	// fmt.Println("size", unsafe.Sizeof(info))
-
-	ioctl(dev.Fd, EVIOCGID, unsafe.Pointer(&info))
-	ioctl(dev.Fd, EVIOCGNAME, unsafe.Pointer(name))
-	ioctl(dev.Fd, EVIOCGPHYS, unsafe.Pointer(phys))
+	ioctl(dev.File.Fd(), EVIOCGID, unsafe.Pointer(&info))
+	ioctl(dev.File.Fd(), EVIOCGNAME, unsafe.Pointer(name))
+	ioctl(dev.File.Fd(), EVIOCGPHYS, unsafe.Pointer(phys))
 
 	dev.Name = string(name[:])
 	dev.Phys = string(phys[:])
@@ -117,7 +154,7 @@ type device_info struct {
 	bustype, vendor, product, version uint16
 }
 
-
+// Return the keys of a map as a slice (dict.keys())
 func keys (cap *map[int][]int) []int {
 	slice := make([]int, 0)
 
--- events.go
+++ events.go
@@ -2,12 +2,63 @@ package evdev
 
 import (
 	"syscall"
+	"unsafe"
+	"fmt"
 )
 
-
 type InputEvent struct {
-	time syscall.Timeval
-	etype uint16
-	code  uint16
-	value int32
+	Time syscall.Timeval  // time in seconds since epoch at which event occurred
+	Type  uint16  // event type - one of ecodes.EV_*
+	Code  uint16  // event code related to the event type
+	Value int32   // event value related to the event type
+}
+
+func (ev *InputEvent) String() string {
+	return fmt.Sprintf("event at %d.%d, code %02d, type %02d, val %02d",
+		   ev.Time.Sec, ev.Time.Usec, ev.Code, ev.Type, ev.Value)
+}
+
+var eventsize = int(unsafe.Sizeof(InputEvent{}))
+
+
+type KeyEventState uint8
+const (
+	KeyUp KeyEventState = 0x0
+	KeyDown KeyEventState = 0x1
+	KeyHold KeyEventState = 0x2
+)
+
+// KeyEvents are used to describe state changes of keyboards, buttons,
+// or other key-like devices.
+type KeyEvent struct {
+	Event *InputEvent
+	Scancode uint16
+	Keycode  uint16
+	State KeyEventState
+}
+
+func (kev *KeyEvent) New(ev *InputEvent) {
+	kev.Event = ev
+	kev.Keycode = 0 // :todo
+	kev.Scancode = ev.Code
+
+	switch ev.Value {
+	case 0: kev.State = KeyUp
+	case 2: kev.State = KeyHold
+	case 1: kev.State = KeyDown
+	}
+}
+
+func (kev *KeyEvent) String() string {
+	state := "unknown"
+
+	switch kev.State {
+	case KeyUp: state = "up"
+	case KeyHold: state = "hold"
+	case KeyDown: state = "down"
+	}
+
+	return fmt.Sprintf("key event at %d.%d, %d (%d), ()",
+		               kev.Event.Time.Sec, kev.Event.Time.Usec,
+		               kev.Scancode, kev.Event.Code, state)
 }
